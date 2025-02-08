dd946ec0be5ebafb9b277038626b2737c1d182f6
Felipe Botelho <felipe.botelho@evolutap.com.br>
2025-02-07 23:12:17 -0300

    initial commit

---
+++ recipes/provision.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Deployer;
+
+// require __DIR__ . '/provision/databases.php';
+require __DIR__ . '/provision/php.php';
+require __DIR__ . '/provision/user.php';
+require __DIR__ . '/provision/setup-nginx.php';
+// require __DIR__ . '/provision/website.php';
+
+// use Deployer\Task\Context;
+
+// use function Deployer\Support\parse_home_dir;
+
+// add('recipes', ['provision']);
+
+// desc('Provision the server');
+task('provision', [
+    'provision:check',
+    'provision:configure',
+    'provision:update',
+    'provision:upgrade',
+    'provision:install',
+    'provision:ssh',
+    'provision:firewall',
+    'provision:setup-nginx',
+    'provision:user',
+    'provision:php',
+    'provision:node',
+    'provision:databases',
+    'provision:composer',
+    'provision:server',
+    'provision:website',
+    'provision:verify',
+]);
+
+task('provision:test', function () {
+    $domainConf = parse(file_get_contents(__DIR__ . '/provision/nginx/domain.conf'));
+
+    var_dump($domainConf);
+    die();
+});
+
+desc('Adds repositories and update');
+task('provision:update', function () {
+    set('remote_user', get('provision_user'));
+
+    // PHP
+    run('apt-add-repository ppa:ondrej/php -y', ['env' => ['DEBIAN_FRONTEND' => 'noninteractive']]);
+
+    // Update
+    run('apt-get update', ['env' => ['DEBIAN_FRONTEND' => 'noninteractive']]);
+})
+    ->oncePerNode()
+    ->verbose();
+
+desc('Upgrades all packages');
+task('provision:upgrade', function () {
+    set('remote_user', get('provision_user'));
+    run('apt-get upgrade -y', ['env' => ['DEBIAN_FRONTEND' => 'noninteractive'], 'timeout' => 900]);
+})
+    ->oncePerNode()
+    ->verbose();
+
+desc('Installs packages');
+task('provision:install', function () {
+    set('remote_user', get('provision_user'));
+    $packages = [
+        'acl',
+        'apt-transport-https',
+        'build-essential',
+        'nginx',
+        'certbot',
+        'curl',
+        'debian-archive-keyring',
+        'debian-keyring',
+        'fail2ban',
+        'gcc',
+        'git',
+        'libmcrypt4',
+        'libpcre3-dev',
+        'libsqlite3-dev',
+        'make',
+        'ncdu',
+        'nodejs',
+        'pkg-config',
+        'python-is-python3',
+        'redis',
+        'sendmail',
+        'sqlite3',
+        'ufw',
+        'unzip',
+        'uuid-runtime',
+        'whois',
+    ];
+    run('apt-get install -y ' . implode(' ', $packages), ['env' => ['DEBIAN_FRONTEND' => 'noninteractive'], 'timeout' => 900]);
+})
+    ->verbose()
+    ->oncePerNode();
---
+++ recipes/provision/setup-nginx.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Deployer;
+
+
+desc('Sets up Nginx');
+task('provision:setup-nginx', function () {
+    // set('remote_user', get('provision_user'));
+
+    $nginxConf = file_get_contents(__DIR__ . '/nginx/nginx.conf');
+
+    run("echo $'$nginxConf' > /etc/nginx/nginx.conf");
+
+    run("mkdir -p /etc/nginx/nginxconfig.io");
+
+    $generalConf = file_get_contents(__DIR__ . '/nginx/nginxconfig.io/general.conf');
+    $letsencryptConf = file_get_contents(__DIR__ . '/nginx/nginxconfig.io/letsencrypt.conf');
+    $fastcgiConf = file_get_contents(__DIR__ . '/nginx/nginxconfig.io/php_fastcgi.conf');
+    $securityConf = file_get_contents(__DIR__ . '/nginx/nginxconfig.io/security.conf');
+
+    run("echo $'$generalConf' > /etc/nginx/nginxconfig.io/general.conf");
+    run("echo $'$letsencryptConf' > /etc/nginx/nginxconfig.io/letsencrypt.conf");
+    run("echo $'$fastcgiConf' > /etc/nginx/nginxconfig.io/php_fastcgi.conf");
+    run("echo $'$securityConf' > /etc/nginx/nginxconfig.io/security.conf");
+
+    run('mkdir -p /etc/nginx/sites-available');
+
+    $domainConf = parse(file_get_contents(__DIR__ . '/nginx/domain.conf'));
+
+    var_dump($domainConf);
+    die();
+
+    run('systemctl enable nginx');
+    run('systemctl start nginx');
+})
+    ->oncePerNode()
+    ->verbose();
